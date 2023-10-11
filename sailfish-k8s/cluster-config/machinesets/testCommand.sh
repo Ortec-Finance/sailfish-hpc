9c8c82c033177edff726d382e552f1af21bfb988
Zeid Adabel <Zeid.Adabel@ortec-finance.com>
2023-10-11 11:00:28 +0200

    removing test file

--- sailfish-k8s/cluster-config/machinesets/testCommand.sh
+++
@@ -1,3 +0,0 @@
-helm template . --set clusterName=pearl-tst-s4pgx,networkResourceGroup=rg-pearl-001,clusterVnet=pearl-tst-vnet,aroResourceGroup=rg-aro-pearl-001,vmSize=Standard_D16as_v5 > output.yaml
-
-oc apply -f output.yaml
